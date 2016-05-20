f3e0806ca776c2c4cb3752ba2b316e5bde4b1ce3
Carlos C Barajas Jr <soldotsol@gmail.com>
2016-05-19 20:30:43 -0700

    Initial setup, test valid string format

---
+++ PkgInstaller.UnitTests/PackageInstallerTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using PkgInstaller;
+using System.Linq;
+using System.Collections.Generic;
+using FluentAssertions;
+
+namespace PkgInstaller.Tests
+{
+    [TestFixture()]
+    [Parallelizable]
+    public class PackageInstallerTests
+    {
+        private List<string> simplePackages;
+        private List<string> validPackages;
+        private List<string> invalidPackages;
+
+        [SetUp]
+        public void InitPackages()
+        {
+            simplePackages = new List<string> {
+                "KittenService: CamelCaser",
+                "CamelCaser: "
+            };
+            validPackages = new List<string> {
+                "KittenService: ",
+                "Leetmeme: Cyberportal",
+                "Cyberportal: Ice",
+                "CamelCaser: KittenService",
+                "Fraudstream: Leetmeme",
+                "Ice: "
+            };
+            invalidPackages = new List<string> {
+                "KittenService: ",
+                "Leetmeme: Cyberportal",
+                "Cyberportal: Ice",
+                "CamelCaser: KittenService",
+                "Fraudstream: ",
+                "Ice: Leetmeme"
+            };
+        }
+
+        [Test()]
+        public void Given_A_Valid_String_When_ValidateInput_Then_Returns_True()
+        {
+            var installer = new PackageInstaller();
+
+            var result = installer.ValidateInput(simplePackages.First());
+
+            result.Should().BeTrue();
+        }
+    }
+}
---
+++ PkgInstaller/PackageInstaller.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace PkgInstaller
+{
+    public class PackageInstaller
+    {
+        public List<string> Packages { get; set; }
+
+        public bool ValidateInput(string package)
+        {
+            return package.Split(new string[] { ": " }, System.StringSplitOptions.None).Length > 1;            
+        }
+    }
+}
